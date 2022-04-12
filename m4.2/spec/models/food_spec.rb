0e4f06e8501f2b04203fefd29fe18b1c34548bab
Bariq <bariqhibat@gmail.com>
2022-04-13 01:15:20 +0800

    fix tests for category FK
    Signed-off-by: Bariq <bariqhibat@gmail.com>

--- m4.2/spec/models/food_spec.rb
+++ m4.2/spec/models/food_spec.rb
@@ -1,61 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  it 'is valid with a name and a description' do
-    food = Food.new(
-      name: 'Nasi Uduk',
-      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
-      price: 15000.0
+  let(:category) {
+    return Category.new(name: "dummy")
+  }
+  subject(:food) { 
+    return Food.new(
+      name: 'Test',
+      description: "Bla",
+      price: 15000.0,
+      category: category,
     )
+ }
 
+  it 'is valid with a name and a description' do
     expect(food).to be_valid
   end
 
-  it 'is invalid without a name' do
-    food = Food.new(
-      name: nil,
-      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
-      price: 15000.0
-    )
-
+  it 'is invalid without a category' do
+    food.category = nil
     food.valid?
+    expect(food.errors[:category]).to include("must exist")
+  end
 
+  it 'is invalid without a name' do
+    food.name = nil
+    food.valid?
     expect(food.errors[:name]).to include("can't be blank")
   end
 
   it 'is invalid without a blank name' do
-    food = Food.new(
-      name: "",
-      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
-      price: 15000.0
-    )
-
+    food.name = ""
     food.valid?
-
     expect(food.errors[:name]).to include("can't be blank")
   end
   
   it 'is invalid without a description' do
-    food = Food.new(
-      name: "Name",
-      description: nil,
-      price: 15000.0
-    )
-
+    food.description = nil
     food.valid?
-
     expect(food.errors[:description]).to include("can't be blank")
   end
 
   it 'is invalid without a blank description' do
-    food = Food.new(
-      name: "Name",
-      description: '',
-      price: 15000.0
-    )
-
+    food.description = ""
     food.valid?
-
     expect(food.errors[:description]).to include("can't be blank")
   end
 
@@ -63,13 +51,15 @@ RSpec.describe Food, type: :model do
     food1 = Food.create(
       name: "Nasi Uduk",
       description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
-      price: 10000.0
+      price: 10000.0,
+      category: category
     )
     
     food2 = Food.new(
       name: "Nasi Uduk",
       description: "Just with a different description.",
-      price: 10000.0
+      price: 10000.0,
+      category: category
     )
 
     food2.valid?
@@ -78,24 +68,14 @@ RSpec.describe Food, type: :model do
   end
 
   it "is invalid with a non-integer price" do
-    food = Food.create(
-      name: "Nasi Uduk",
-      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
-      price: "price"
-    )
-
+    food.price = "test_price"
     food.valid?
 
     expect(food.errors[:price]).to include("is not a number")
   end
 
   it "is invalid with a price less than 0.01" do
-    food = Food.create(
-      name: "Nasi Uduk",
-      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
-      price: -0.001
-    )
-
+    food.price = -5000.0
     food.valid?
 
     expect(food.errors[:price]).to include("must be greater than 0.01")
@@ -106,19 +86,22 @@ RSpec.describe Food, type: :model do
       food1 = Food.create(
         name: "Nasi Uduk",
         description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
-        price: 10000.0
+        price: 10000.0,
+        category: category
       )
 
       food2 = Food.create(
         name: "Kerak Telor",
         description: "Betawi traditional spicy omelette made from glutinous rice cooked with egg and served with serundeng.",
-        price: 8000.0
+        price: 8000.0,
+        category: category
       )
 
       food3 = Food.create(
         name: "Nasi Semur Jengkol",
         description: "Based on dongfruit, this menu promises a unique and delicious taste with a small hint of bitterness.",
-        price: 8000.0
+        price: 8000.0,
+        category: category
       )
 
       expect(Food.by_letter("N")).to eq([food3, food1])
