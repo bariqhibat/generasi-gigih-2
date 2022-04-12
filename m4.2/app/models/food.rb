4206eca5a071e2638eb158c475cebd449000c558
Bariq <bariqhibat@gmail.com>
2022-04-13 00:49:38 +0800

    add new category model 1:N food, add new method to search category
    Signed-off-by: Bariq <bariqhibat@gmail.com>

--- m4.2/app/models/food.rb
+++ m4.2/app/models/food.rb
@@ -1,4 +1,5 @@
 class Food < ApplicationRecord
+    belongs_to :category
     validates :name, presence: true, uniqueness: true
     validates :description, presence: true
     validates :price, numericality: true
@@ -6,5 +7,9 @@ class Food < ApplicationRecord
 
     def self.by_letter(letter)
         where("name LIKE ?", "#{letter}%").order(:name)
+    end
+
+    def self.by_category(category)
+        left_outer_joins(:category).where(categories: {name: "#{category}"})
     end
 end
--- m4.2/db/schema.rb
+++ m4.2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_12_162612) do
+ActiveRecord::Schema.define(version: 2022_04_12_163830) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 2022_04_12_162612) do
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_foods_on_category_id"
   end
 
+  add_foreign_key "foods", "categories"
 end
