8ffb7841ec515bee880b5c0bc7295a063800182c
Bariq <bariqhibat@gmail.com>
2022-04-13 22:11:21 +0800

    add faker and solargraph
    Signed-off-by: Bariq <bariqhibat@gmail.com>

--- m4.3/spec/factories/foods.rb
+++ m4.3/spec/factories/foods.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :food do
-    name { 'Nasi Uduk' }
-    description { 'Betawi style steamed rice cooked in coconut milk. Delicious!' }
-    price { 10_000.0 }
+    name { Faker::Food.dish }
+    description { Faker::Food.description }
+    price { Faker::Number.positive }
     category {Category.first || association(:category)}  # ref: https://qiita.com/Kolosek/items/7c85337f928161e0e76e
   end
 end
